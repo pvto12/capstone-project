c875770e2eb4ee6c3f7d3f9697f1c070e461eda6
araborg <arababs2015@gmail.com>
2025-11-05 00:21:22 +0100

    septa_capstone_project: 27

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+
+const openAndClose = () => {
+	const open = false;
+
+	// hamburger.onclick
+};
+
+hamburger.addEventListener("click", openAndClose);
