a8bc6cef4f8e75a348c7415771d008fcb899b32f
araborg <arababs2015@gmail.com>
2025-11-05 02:54:02 +0100

    septa_capstone_project: 44

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-const hamburger = document.querySelector(".hamburger");
-const unorderedList = document.querySelector(".show-or-hide");
-let menuOpen = false;
+// const hamburger = document.querySelector(".hamburger");
+// const unorderedList = document.querySelector(".show-or-hide");
+// let menuOpen = false;
 
-const openAndClose = () => {
-	if (!menuOpen) {
-		unorderedList.style.display = "block";
-		menuOpen = true;
-	} else if (menuOpen) {
-		unorderedList.style.display = "none";
-		menuOpen = false;
-	}
-};
+// const openAndClose = () => {
+// 	if (!menuOpen) {
+// 		unorderedList.style.display = "block";
+// 		menuOpen = true;
+// 	} else if (menuOpen) {
+// 		unorderedList.style.display = "none";
+// 		menuOpen = false;
+// 	}
+// };
 
-hamburger.addEventListener("click", openAndClose);
+// hamburger.addEventListener("click", openAndClose);
