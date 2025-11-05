2adf16b1e1c88e3c300ca10d2ac7aa1d68614bf9
araborg <arababs2015@gmail.com>
2025-11-05 02:03:41 +0100

    septa_capstone_project: 38

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
+const unorderedList = document.querySelector("ul");
+
+console.log(hamburger, unorderedList);
 
 const openAndClose = () => {
 	const open = false;
