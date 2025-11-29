23d55d1ffe8ae410ecdcb0794b6c9d7c41ad840a
araborg <arababs2016@gmail.com>
2025-11-29 17:16:11 +0100

    done with countdown 8

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+const celebrant = document.querySelector("#celebrant");
+const sender = document.querySelector("#sender");
+const wishes = document.querySelector("#wishes");
+
+const btn = document.querySelector("button");
+
+console.log(celebrant, sender, wishes, btn);
