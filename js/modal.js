49e5e79a5bb640648ee3ea0aab7f66e8a2bdfea0
araborg <arababs2016@gmail.com>
2025-12-01 09:31:20 +0100

    wishes form: 15

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,14 @@
+const closeBtn = document.querySelector(".close-form");
+const formContainer = document.querySelector(".form-container");
+
 const celebrant = document.querySelector("#celebrant");
 const sender = document.querySelector("#sender");
 const wishes = document.querySelector("#wishes");
 
 const btn = document.querySelector("button");
 
-console.log(celebrant, sender, wishes, btn);
+closeBtn.addEventListener("click", () => {
+	console.log("clicked");
+
+	formContainer.style.display = "none";
+});
