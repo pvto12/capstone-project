34b8589cb76c906a22bfefa3f21049cb83514543
araborg <arababs2016@gmail.com>
2025-12-01 22:31:24 +0100

    updated form: 5

--- js/modal.js
+++ js/modal.js
@@ -9,8 +9,8 @@ const celebrant = document.querySelector("#celebrant");
 const sender = document.querySelector("#sender");
 const wishes = document.querySelector("#wishes");
 
-// navbar
-// const navbar = document.querySelector(".show-or-hide");
+const birthdays = localStorage.getItem("day") || [];
+console.log(birthdays);
 
 closeBtn.addEventListener("click", () => {
 	formContainer.style.display = "none";
@@ -25,11 +25,34 @@ teamMember.addEventListener("click", () => {
 formContainer.addEventListener("submit", (e) => {
 	e.preventDefault();
 
+	// get d input values
+	const salutation = document.querySelector("#salutation").value;
+	const firstname = document.querySelector("#firstname").value;
+	const lastname = document.querySelector("#lastname").value;
+	const gender = document.querySelector("#gender").value;
+	const dob = document.querySelector("#dob").value;
+
+	const createBirthdate = {
+		id: new Date().getTime(),
+		salutation,
+		firstname,
+		lastname,
+		gender,
+		dob,
+	};
+
+	birthdays.push(createBirthdate);
+	birthdays.sort((a, b) => b.id - a.id);
+
+	localStorage.setItem("day", JSON.stringify(createBirthdate));
+
 	formContainer.style.display = "none";
 });
 
 function init() {
 	formContainer.style.display = "none";
+
+	localStorage.getItem("day");
 }
 
 init();
