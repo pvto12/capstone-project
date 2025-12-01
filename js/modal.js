ad3c79a5124eda13947bbcf3d9186c1fea36a4dd
araborg <arababs2016@gmail.com>
2025-12-01 09:55:00 +0100

    wishes form: 16

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,7 @@
+const teamMember = document.querySelector(".team-member");
+
+const birthdayForm = document.querySelector(".birthday-wishes");
+
 const closeBtn = document.querySelector(".close-form");
 const formContainer = document.querySelector(".form-container");
 
@@ -5,10 +9,36 @@ const celebrant = document.querySelector("#celebrant");
 const sender = document.querySelector("#sender");
 const wishes = document.querySelector("#wishes");
 
-const btn = document.querySelector("button");
+function closeForm() {
+	closeBtn.addEventListener("click", () => {
+		// console.log("clicked");
 
-closeBtn.addEventListener("click", () => {
+		formContainer.style.display = "none";
+	});
+}
+
+closeForm();
+
+teamMember.addEventListener("click", () => {
+	formContainer.style.display = "block";
+
+	// console.log(formContainer);
+
+	// let;
+});
+
+formContainer.addEventListener("submit", (e) => {
+	e.preventDefault();
+
 	console.log("clicked");
-
 	formContainer.style.display = "none";
 });
+
+// 	// formContainer.style.display = "none";
+// });
+
+function init() {
+	formContainer.style.display = "none";
+}
+
+init();
